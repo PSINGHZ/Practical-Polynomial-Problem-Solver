4ff9b305a66d27467c36b29c622c9c159c38af15
Cotapher Xie <cotapher@yahoo.com>
2015-04-21 09:09:57 -0400

    SWAG

---
+++ src/polynomials/Polynomials.java
@@ -0,0 +1,16 @@
+
+package polynomials;
+
+import java.util.*;
+
+
+public class Polynomials {
+
+
+    public static void main(String[] args) {
+        
+        Polynomial x = new Polynomial(2);
+        
+    }
+    
+}
